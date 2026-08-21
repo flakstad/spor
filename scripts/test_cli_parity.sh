c7b89ec950f150651b0cc9b3db6343e3fa8bad9d
Andreas Flakstad <git@andreasflakstad.no>
2026-08-21 21:48:56 +0200

    Normalize embedded Windows CLI store paths

--- scripts/test_cli_parity.sh
+++ scripts/test_cli_parity.sh
@@ -194,8 +194,12 @@ case "$relation_result" in *'"Ivan"'*) ;; *) exit 1 ;; esac
 case "$relation_result" in *'"Petr"'*) exit 1 ;; esac
 
 ALT_DB="$TMP_DIR/alternate.db"
+ALT_DB_EDN="$ALT_DB"
+case "$(uname -s)" in
+  MINGW*|MSYS*|CYGWIN*) ALT_DB_EDN="$(cygpath -m "$ALT_DB")" ;;
+esac
 alternate_result="$($CLI exec --memory \
-  "{:steps [{:id :tx :op :transact :store \"$ALT_DB\"
+  "{:steps [{:id :tx :op :transact :store \"$ALT_DB_EDN\"
              :tx [{:db/id 1 :source/name \"alternate\"}]}
             {:id :q :op :query :db [:ref :tx :db-after]
              :query [:find ?name . :where [1 :source/name ?name]]}]
@@ -203,22 +207,22 @@ alternate_result="$($CLI exec --memory \
 test "$alternate_result" = '"alternate"'
 
 alternate_db="$($CLI exec --memory \
-  "{:steps [{:id :db :op :db :store \"$ALT_DB\"}]
+  "{:steps [{:id :db :op :db :store \"$ALT_DB_EDN\"}]
     :return [:ref :db]}")"
 case "$alternate_db" in *':basis-t 1'*) ;; *) exit 1 ;; esac
 
 alternate_reopened="$($CLI exec --memory \
-  "{:steps [{:id :tx :op :transact :store \"$ALT_DB\"
+  "{:steps [{:id :tx :op :transact :store \"$ALT_DB_EDN\"
              :tx [{:db/id 2 :source/name \"reopened\"}]}
-            {:id :db :op :db :store \"$ALT_DB\"}
+            {:id :db :op :db :store \"$ALT_DB_EDN\"}
             {:id :q :op :query :db [:ref :db]
              :query [:find ?name . :where [2 :source/name ?name]]}]
     :return {:answer [:ref :q] :tx-db [:ref :tx :db-after]}}")"
 case "$alternate_reopened" in *':answer "reopened"'*':basis-t 2'*) ;; *) exit 1 ;; esac
 
 alternate_maintenance="$($CLI exec --memory \
-  "{:steps [{:id :info :op :index-info :store \"$ALT_DB\" :index :eavt}
-            {:id :maintain :op :maintain-indexes :store \"$ALT_DB\"
+  "{:steps [{:id :info :op :index-info :store \"$ALT_DB_EDN\" :index :eavt}
+            {:id :maintain :op :maintain-indexes :store \"$ALT_DB_EDN\"
              :max-steps 0}]
     :return {:info [:ref :info] :maintenance [:ref :maintain]}}")"
 case "$alternate_maintenance" in *':index :eavt'*':steps 0'*) ;; *) exit 1 ;; esac
